14b57376f9bc248c19480717f6c2fdba0c84cd39
Bruno Tatsuya <brunomasunaga@gmail.com>
2022-01-29 21:31:15 -0300

    Added sitemap and robots.txt

---
+++ pages/sitemap/index.js
@@ -0,0 +1,25 @@
+import { getServerSideSitemap } from 'next-sitemap'
+
+import { getLastBlogPosts } from '../api/get-last-blog-posts'
+
+export async function getServerSideProps(context) {
+  const posts = await getLastBlogPosts();
+
+  const blogUrls = posts.map((post) => ({loc: 'https://brunotatsuya.dev/blog/' + post.slug,  lastmod: post.datePublished}));
+
+  const standardUrls = [
+    {
+      loc: 'https://brunotatsuya.dev',
+      lastmod: new Date().toISOString(),
+    },
+    {
+      loc: 'https://brunotatsuya.dev/blog',
+      lastmod: new Date().toISOString(),
+    },
+  ]
+
+  return getServerSideSitemap(context, standardUrls.concat(blogUrls))
+}
+
+// Default export to prevent next.js errors
+export default () => {}
