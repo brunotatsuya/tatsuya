93612637c336149ca32e839cc5f1ccfcc16b468c
Bruno Tatsuya <brunomasunaga@gmail.com>
2022-02-04 23:16:32 -0300

    Changed markdown highlight engine

--- components/blog/markdown-render.js
+++ components/blog/markdown-render.js
@@ -1,11 +1,10 @@
 import { ReactMarkdown } from 'react-markdown/lib/react-markdown'
-import rehypeHighlight from 'rehype-highlight'
+import CodeBlock from './code-block'
 import rehypeRaw from 'rehype-raw'
+import remarkGfm from 'remark-gfm'
 
-export default function MarkdownRender({ markdown, highlight = true }) {
-  return highlight ? (
-    <ReactMarkdown className="markdown-body pt-5 pb-4" rehypePlugins={[rehypeRaw, rehypeHighlight]}>{markdown}</ReactMarkdown>
-  ) : (
-    <ReactMarkdown className="markdown-body pt-5 pb-4" rehypePlugins={[rehypeRaw]}>{markdown}</ReactMarkdown>
-  )
+export default function MarkdownRender({ markdown }) {
+  return (
+    <ReactMarkdown className="markdown-body pt-5 pb-4" rehypePlugins={[rehypeRaw]} remarkPlugins={[remarkGfm]} components={CodeBlock}>{markdown}</ReactMarkdown>
+  ) 
 }
